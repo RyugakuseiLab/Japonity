d27f2e8ec7626d57596abb59faac0d63a74b3a5d
izudan <idan3266@gmail.com>
2018-12-16 09:27:11 +0000

    tableを）作成（langとlevel）

---
+++ cms/database/migrations/2018_12_16_081458_create_levels_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLevelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Levels', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('level');
+            $table->string('level_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Levels');
+    }
+}
